dd1eae60d896ba969f3e8da810c4f55ba594ce31
Andrew Bromwich <a.bromwich@gmail.com>
2022-01-10 18:17:10 +1000

    Fix SQL strip function (incorrect escaping of newlines)

--- lib/polymorphic_constraints/utils/sql_string.rb
+++ lib/polymorphic_constraints/utils/sql_string.rb
@@ -2,7 +2,7 @@ module PolymorphicConstraints
   module Utils
     module SqlString
       def strip_non_essential_spaces(string_to_strip)
-        string_to_strip.gsub(/\s{2,}|\\n/, " ").strip
+        string_to_strip.gsub(/\s{2,}|\\n/, ' ').strip
       end
     end
   end
